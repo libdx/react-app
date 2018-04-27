94b32af7c76225af234e42430b4836adc7489a45
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-04-28 00:14:06 +0200

    Preliminary design of Movie Card
     - Make `MovieCard` component based on Boostrap's cards
     - Add `common.css` file

--- src/components/movie_card.js
+++ src/components/movie_card.js
@@ -2,17 +2,28 @@
 
 import React, { Component } from 'react'
 import Movie from '../movie'
+import '../styles/common.css'
 
 type MovieCardProps = {
     movie: Movie
 }
 
-const MovieCard = (props: MovieCardProps) => {
-    // TODO: provide planceholder if image loading if failed
+const MovieCard = ({movie}: MovieCardProps) => {
+    // TODO: provide planceholder if image loading failed
 
     return (
-        <div>
-            <img src={props.movie.imageURL} />
+        <div className="card movie-card">
+            <img className="card-img-top" src={movie.imageURL} />
+            <div className="card-body movie-card-body">
+                <h6 className="card-title textbox">
+                    <span className="alignleft">{movie.title}</span>
+                    <span className="badge badge-light alignright">{movie.releaseYear}</span>
+                    <div className="alighclear" />
+                </h6>
+                <p className="card-subtitle mb-2 text-muted">
+                    {movie.gener}
+                </p>
+            </div>
         </div>
     )
 }
