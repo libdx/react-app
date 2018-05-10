7905812aa43b140150fb91dd855e0ea1f8567fcd
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-05-10 16:44:36 +0200

    Add test on `Broken`

---
+++ src/components/__tests__/broken.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Broken from '../broken'
+
+describe('Broken', () => {
+    it('throws when used', () => {
+        expect(Broken).toThrow()
+    })
+})
+
