e5192f74c54da37806f737a6aef37b6f188d0f5b
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-05-11 22:34:11 +0200

    Add tests on `IndexPage`

---
+++ src/pages/__tests__/index_page.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import IndexPage, { criteria, buttonRecords } from '../index_page'
+
+describe('IndexPage ', () => {
+
+    let component
+
+    beforeEach(() => {
+        component = shallow(<IndexPage />)
+    })
+
+    it('propagates term state to search panel', () => {
+        const term = 'abcd'
+        component.setState({ term })
+
+        const panel = component.find('SearchPanel')
+        expect(panel.props().term).toEqual(term)
+    })
+
+    it('propagates criteria search by state to button group', () => {
+        component.setState({ searchBy: buttonRecords[1].id })
+
+        const buttonGroup = component.find('ButtonGroup')
+        expect(buttonGroup.props().selectedButtonId).toEqual(buttonRecords[1].id)
+    })
+})
