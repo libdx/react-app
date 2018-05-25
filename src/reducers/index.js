de6f011f34a8cfdec34de5eede21eb1320a825b0
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-05-25 16:59:19 +0200

    Rename `reducers/sort_order` to `reducers/sorting`

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import fetchMovies from './fetch_movies'
 import searchMovies from './search_movies'
-import sortKey from './sort_order'
+import sortKey from './sorting'
 
 const rootReducer = combineReducers({
     movies: fetchMovies,
