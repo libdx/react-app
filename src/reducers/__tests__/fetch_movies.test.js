5e38c3a516bd4873d1d143fa1ec53ffd8aa37a1c
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-06-04 22:00:23 +0200

    Add tests on `fetchMovies` reducer

---
+++ src/reducers/__tests__/fetch_movies.test.js
@@ -0,0 +1,31 @@
+import { FETCH_MOVIES } from '../../actions'
+import { fetchMovies } from '../../actions/fetch_movies'
+import reducer from '../fetch_movies'
+
+describe('fetchMovies reducer', () => {
+    it('returns default state', () => {
+        expect(reducer(undefined, {})).toEqual([])
+    })
+
+    it('handles FETCH_MOVIES action', () => {
+        const values = [1, 2, 3]
+        const data = { data: values }
+        const action = {
+            type: FETCH_MOVIES,
+            payload: { data }
+        }
+        expect(reducer(null, action)).toEqual(values)
+    })
+
+    it('updates (replaces) old state', () => {
+        const values = [41, 42, 43]
+        const data = { data: values }
+        const action = {
+            type: FETCH_MOVIES,
+            payload: { data }
+        }
+
+        const oldState = [1, 2, 3]
+        expect(reducer(oldState, action)).toEqual(values)
+    })
+})
