644e0d4449d15e3f0b42192c694500ddde5d87ea
Oleksandr Ignatenko <oleksandr_ignatenko@epam.com>
2018-06-04 22:38:55 +0200

    Add more test on `StatusBar` to cover it 100%

--- src/components/__tests__/status_bar.test.js
+++ src/components/__tests__/status_bar.test.js
@@ -6,16 +6,48 @@ import StatusBar from '../status_bar'
 describe('StatusBar', () => {
 
     const title = "A Title"
+    const sortOptions = [
+        {id: 'RELEASE_DATE', title: 'release date'},
+        {id: 'RATING', title: 'rating'}
+    ]
     let element
     let component
+    let onOptionClick
 
     beforeEach(() => {
-        element = <StatusBar title={title} />
+        onOptionClick = jest.fn()
+        element = <StatusBar
+            title={title} 
+            options={sortOptions}
+            activeOptionID={sortOptions[0].id}
+            onOptionClick={onOptionClick}
+        />
         component = shallow(element)
     })
 
-    it('show correct title', () => {
+    it('shows correct title', () => {
         expect(component.text()).toEqual(expect.stringContaining(title))
+    })
+
+    it('shows sort options', () => {
+        expect(component.text()).toEqual(expect.stringContaining(sortOptions[0].title))
+        expect(component.text()).toEqual(expect.stringContaining(sortOptions[1].title))
+    })
+
+    it('shows active option', () => {
+        const option = component.find('.sort-opt-active')
+        expect(option.exists()).toBe(true)
+    })
+
+    it('shows inactive option', () => {
+        const option = component.find('.sort-opt')
+        expect(option.exists()).toBe(true)
+    })
+
+    it('passes correct id on click', () => {
+        const option = component.find('.sort-opt')
+        option.simulate('click')
+        expect(onOptionClick).toBeCalledWith(sortOptions[1].id)
     })
 
     it('renders correctly', () => {
